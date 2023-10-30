a47a850dbae8a2000b30cf82f1dd746e6b3a2d55
Thomis Garrido <garridothomis@yahoo.com>
2023-10-29 22:57:58 -0400

    Creacion de carpetas

--- Database/DTO/Graduate.cs
+++ Database/DTO/Graduate.cs
@@ -1,7 +1,8 @@
 public class Graduate
 {
     public int id { get; set; }
-    public int ParticipanteId { get; set; }
+
+    public int ParticipanteId {get; set;}
     public String? Identificacion { get; set; }
     public string? PrimerNombre { get; set; }
     public string? SegundoNombre { get; set; }
@@ -9,6 +10,9 @@ public class Graduate
     public string? SegundoApellido { get; set; }
     public char Genero { get; set; }
     public string? FechaNac { get; set; }
+    public string? FotoPerfilUrl { get; set; }
+    public string? About { get; set; }
+    public Boolean? Destacado { get; set; }
     public string? MatriculaGrado { get; set; }
     public string? MatriculaEgresado { get; set; }
     public Nationality? Nacionalidad { get; set; }
